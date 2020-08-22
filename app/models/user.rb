3d723fcf1b837b6c756032c89209ec0f850e5648
Nayan Gupta <neema.nayan93@gmail.com>
2020-08-23 00:19:24 +0530

    Devise view added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  CustomRole = {teacher: "Teacher", student: "Student"}
+  AllRole = {admin: "Admin"}.merge(CustomRole)
+
+  enum role: AllRole.keys
+
+  validates_presence_of :role
+
 end
